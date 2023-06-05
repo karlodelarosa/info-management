443f3816665e71a491708907e5bc021862454cdb
Karlo Dela Rosa <jrosa@salespond.com>
2023-06-06 00:56:07 +0800

    Update color theme

--- src/components/nav/TopNav.tsx
+++ src/components/nav/TopNav.tsx
@@ -4,7 +4,7 @@ import '@/assets/styles/module/navigation.css'
 
 export default function TopNav() {
   return (
-    <div className="fixed top-0 left-0 z-50 w-full text-gray-700 bg-gray-400 dark-mode:text-gray-200 ">
+    <div className="fixed top-0 left-0 z-50 w-full text-gray-700 bg-white dark-mode:text-gray-200 border-b border-gray-300">
       <div className="flex flex-col px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div className="p-4 flex flex-row items-center justify-between">
           <Brand />
